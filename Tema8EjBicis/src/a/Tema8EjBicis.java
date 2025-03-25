e8e3bc9ecd2ffb1208dad0a3870c77fb0de65bba
ALVARO <alvaroch975@gmail.com>
2025-03-25 11:25:10 +0100

    sigma aprueba examen yo si Signed-off-by: ALVARO <alvaroch975@gmail.com>

--- Tema8EjBicis/src/a/Tema8EjBicis.java
+++ Tema8EjBicis/src/a/Tema8EjBicis.java
@@ -3,9 +3,12 @@ package a;
 import java.awt.EventQueue;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -15,7 +18,9 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 class ConnectionSingleTon {
 	private static Connection con;
@@ -35,9 +40,20 @@ class ConnectionSingleTon {
 public class Tema8EjBicis {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField textField_Name;
+	private JTextField textField_Edad;
+	private JTextField textField_CB;
+
+	boolean comprobarExpReg(String cadena, String patron) {
+		Pattern pat = Pattern.compile(patron);
+		Matcher mat = pat.matcher(cadena);
+
+		if (mat.matches()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	/**
 	 * Launch the application.
@@ -71,30 +87,6 @@ public class Tema8EjBicis {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JButton btnAadirUsuario = new JButton("Añadir Usuario");
-		btnAadirUsuario.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnAadirUsuario.setBounds(172, 60, 154, 25);
-		frame.getContentPane().add(btnAadirUsuario);
-
-		JButton btnAñadirBici = new JButton("Añadir Bici");
-		btnAñadirBici.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnAñadirBici.setBounds(578, 60, 155, 25);
-		frame.getContentPane().add(btnAñadirBici);
-
-		JButton btnNewAlquilar = new JButton("Alquilar");
-		btnNewAlquilar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewAlquilar.setBounds(371, 27, 154, 25);
-		frame.getContentPane().add(btnNewAlquilar);
-
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("ID_usuario");
 		model.addColumn("Nombre");
@@ -114,9 +106,7 @@ public class Tema8EjBicis {
 
 				model.addRow(row);
 			}
-		}
-
-		catch (SQLException ex) {
+		} catch (SQLException ex) {
 		}
 
 		JTable table = new JTable(model);
@@ -125,8 +115,8 @@ public class Tema8EjBicis {
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(76, 112, 367, 188);
 		frame.getContentPane().add(scrollPane);
-		
-		//Tabla2
+
+		// Tabla2
 		DefaultTableModel model2 = new DefaultTableModel();
 		model2.addColumn("ID_Bici");
 		model2.addColumn("Usuario_Asignado");
@@ -139,7 +129,6 @@ public class Tema8EjBicis {
 				Object[] row1 = new Object[2];
 				row1[0] = rs1.getInt("ID_Bici");
 				row1[1] = rs1.getInt("Usuario_Asignado");
-
 
 				model2.addRow(row1);
 			}
@@ -154,34 +143,147 @@ public class Tema8EjBicis {
 		JScrollPane scrollPane1 = new JScrollPane(table1);
 		scrollPane1.setBounds(467, 112, 367, 188);
 		frame.getContentPane().add(scrollPane1);
-		
+
 		JLabel lblNombre = new JLabel("Nombre:");
 		lblNombre.setBounds(76, 334, 70, 15);
 		frame.getContentPane().add(lblNombre);
-		
+
 		JLabel lblEdad = new JLabel("Edad:");
 		lblEdad.setBounds(76, 366, 70, 15);
 		frame.getContentPane().add(lblEdad);
-		
+
 		JLabel lblCuentaBancaria = new JLabel("Cuenta Bancaria:");
 		lblCuentaBancaria.setBounds(76, 393, 125, 15);
 		frame.getContentPane().add(lblCuentaBancaria);
-		
-		textField = new JTextField();
-		textField.setBounds(164, 332, 114, 19);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(164, 364, 114, 19);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(212, 391, 114, 19);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-		
+
+		textField_Name = new JTextField();
+		textField_Name.setBounds(164, 332, 114, 19);
+		frame.getContentPane().add(textField_Name);
+		textField_Name.setColumns(10);
+
+		textField_Edad = new JTextField();
+		textField_Edad.setBounds(164, 364, 114, 19);
+		frame.getContentPane().add(textField_Edad);
+		textField_Edad.setColumns(10);
+
+		textField_CB = new JTextField();
+		textField_CB.setBounds(212, 391, 114, 19);
+		frame.getContentPane().add(textField_CB);
+		textField_CB.setColumns(10);
+
+		JButton btnComprobarYFinalizar = new JButton("Comprobar y finalizar");
+		btnComprobarYFinalizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField_Name.getText().length() == 0) {
+					JOptionPane.showMessageDialog(frame, "El nombre está vacío", "Advertencia",
+							JOptionPane.ERROR_MESSAGE);
+				} else if (textField_Edad.getText().length() == 0) {
+					JOptionPane.showMessageDialog(frame, "La edad está vacía", "Advertencia",
+							JOptionPane.ERROR_MESSAGE);
+				} else if (textField_CB.getText().length() == 0) {
+					JOptionPane.showMessageDialog(frame, "El número de cuenta está vacío", "Advertencia",
+							JOptionPane.ERROR_MESSAGE);
+				}
+				if (!comprobarExpReg(textField_Name.getText(), "^[a-zA-z]+$")) {
+					JOptionPane.showMessageDialog(frame, "El nombre debe contener solo letras", "Advertencia",
+							JOptionPane.ERROR_MESSAGE);
+				} else if (!comprobarExpReg(textField_Edad.getText(), "^\\d{1,3}$")) {
+					JOptionPane.showMessageDialog(frame, "La edad debe ser un número entero de hasta 3 dígitos",
+							"Advertencia", JOptionPane.ERROR_MESSAGE);
+				} else if (!comprobarExpReg(textField_CB.getText(), "^\\d{1,12}$")) {
+					JOptionPane.showMessageDialog(frame,
+							"La cuenta bancaria debe ser un número entero de hasta 12 dígitos", "Advertencia",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					lblNombre.setVisible(false);
+					lblEdad.setVisible(false);
+					lblCuentaBancaria.setVisible(false);
+					textField_Name.setVisible(false);
+					textField_Edad.setVisible(false);
+					textField_CB.setVisible(false);
+					btnComprobarYFinalizar.setVisible(false);
+
+					try {
+						// Añadir datos usuario
+						Connection con = ConnectionSingleTon.getConnection();
+						PreparedStatement insUs = con
+								.prepareStatement("INSERT INTO Usuarios (Nombre,Edad,Cuenta_Bancaria) VALUES (?,?,?)");
+						String nombre = textField_Name.getText();
+						String edad = textField_Edad.getText();
+						String cb = textField_CB.getText();
+						insUs.setString(1, nombre);
+						insUs.setString(2, edad);
+						insUs.setString(3, cb);
+						insUs.executeUpdate();
+						JOptionPane.showMessageDialog(frame, "Se ha añadido el producto");
+						con.close();
+						insUs.close();
+					} catch (SQLException erra) {
+						erra.printStackTrace();
+						erra.getMessage();
+						erra.getSQLState();
+						JOptionPane.showMessageDialog(frame, erra.getMessage(), "Advertencia",
+								JOptionPane.ERROR_MESSAGE);
+					}	
+				}
+				}
+			});
+		btnComprobarYFinalizar.setBounds(76, 427, 194, 25);
+		frame.getContentPane().add(btnComprobarYFinalizar);
+
+		JButton btnAadirUsuario = new JButton("Añadir Usuario");
+		btnAadirUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblNombre.setVisible(true);
+				lblEdad.setVisible(true);
+				lblCuentaBancaria.setVisible(true);
+				textField_Name.setVisible(true);
+				textField_Edad.setVisible(true);
+				textField_CB.setVisible(true);
+				btnComprobarYFinalizar.setVisible(true);
+			}
+		});
+
+		JButton btnAñadirBici = new JButton("Añadir Bici");
+		btnAñadirBici.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Connection con = ConnectionSingleTon.getConnection();
+					PreparedStatement insPstmt = con.prepareStatement("INSERT INTO Bicis VALUES (null, 0)");
+					insPstmt.executeUpdate();
+					insPstmt.close();
+					JOptionPane.showMessageDialog(frame, "Se ha añadido el producto");
+					con.close();
+					insPstmt.close();
+				} catch (SQLException erra) {
+					erra.printStackTrace();
+					erra.getMessage();
+					erra.getSQLState();
+					JOptionPane.showMessageDialog(frame, erra.getMessage(), "Advertencia", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+
+		JButton btnNewAlquilar = new JButton("Alquilar");
+		btnNewAlquilar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewAlquilar.setBounds(371, 27, 154, 25);
+		frame.getContentPane().add(btnNewAlquilar);
+		btnAñadirBici.setBounds(578, 60, 155, 25);
+		frame.getContentPane().add(btnAñadirBici);
+		btnAadirUsuario.setBounds(161, 60, 165, 25);
+		frame.getContentPane().add(btnAadirUsuario);
+
+		lblNombre.setVisible(false);
+		lblEdad.setVisible(false);
+		lblCuentaBancaria.setVisible(false);
+		textField_Name.setVisible(false);
+		textField_Edad.setVisible(false);
+		textField_CB.setVisible(false);
+		btnComprobarYFinalizar.setVisible(false);
+
 	}
 
 }
